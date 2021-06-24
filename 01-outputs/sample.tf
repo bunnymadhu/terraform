90988caea59e143f9039d0fcc57ef72f2a9ee581
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-24 06:33:40 +0530

    make changes in sample.tf

--- 01-outputs/sample.tf
+++ 01-outputs/sample.tf
@@ -1,17 +1,25 @@
-output "sample" {
-  //value = "Hello World"
-  value = var.sample
-}
-
 ## terraform init
 ## terraform plan
 ## terraform apply to avoid ask yes or no to terraform apply -auto-approve
 ## if you want not need terraform destroy
 
+output "sample" {
+  //value = "Hello World"
+  value = var.sample
+}
+## so in this u can give "hai everyone" in  outputs from  variable data
+
 output "sample1" {
-  value = "Hello World1"
+  //value = "Hello World1"
+  value = "${var.sample}"
 }
 
 variable "sample" {
   default = "hai everyone"
 }
+
+## Variable sample can be accessed as var.sample or ${var.sample}, However
+## var.sample is used without quotes
+## ${var.sample} is used within quotes.
+## Usually ${} used only when you combine with some other data
+## Single quotes does not support by terraform at any level. Supports only double quotes.
