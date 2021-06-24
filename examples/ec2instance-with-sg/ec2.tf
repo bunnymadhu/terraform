c2de96f169d2454fa15f92ba7477b65fd1b5ced7
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-24 21:42:18 +0530

    make changes in ec2.tf

--- examples/ec2instance-with-sg/ec2.tf
+++ examples/ec2instance-with-sg/ec2.tf
@@ -2,3 +2,7 @@ resource "aws_instance" "sample" {
   ami                                    = "ami-059e6ca6474628ef0"
   instance_type                     = "t2.micro"
 }
+
+provider "aws" {
+  region                               = "us-east-1"
+}
