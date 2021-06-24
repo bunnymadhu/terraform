18286865aad5e3731be5fbaa1ae63c37737c45b3
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-24 06:55:06 +0530

    make changes in sample.tf

--- 01-outputs/sample.tf
+++ 01-outputs/sample.tf
@@ -22,5 +22,45 @@ variable "sample" {
 ## Variable sample can be accessed as var.sample or ${var.sample}, However
 ## var.sample is used without quotes
 ## ${var.sample} is used within quotes.
-## Usually ${} used only when you combine with some other data
+## if  we use in ${} it can show in underline,So Usually ${} used only when you combine with some other data
+
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
 ## Single quotes does not support by terraform at any level. Supports only double quotes.
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+## List is in always shows in  square brackets in terraform..
+variable "sample4" {
+  default =  [
+    "Hai",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+output "sample4" {
+  value = "var.sample4[1]"
+}
+# here 0 means first line(Hai),1 means Second line (1000)....
+variable "sample5" {
+  default =  {
+    string = "Hai",
+    number = 100,
+    boolean = true
+  }
+}
+# Terraform can also supports the different data types in single list or map variable,Need not to be the same data type...means string,list,boolean is not needed same data..
+output "sample5" {
+  value = var.sample5["boolean"]
+}
