608111d1b983d783f96af7d0c6719121e7d9457d
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-24 06:22:05 +0530

    make changes in sample.tf

--- 01-outputs/sample.tf
+++ 01-outputs/sample.tf
@@ -2,3 +2,11 @@ output "sample" {
   value = "Hello World"
 }
 
+## terraform init
+## terraform plan
+## terraform apply to avoid ask yes or no to terraform apply -auto-approve
+## if you want not need terraform destroy
+
+output "sample1" {
+  value = "Hello World1"
+}
