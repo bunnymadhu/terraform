0134b68ac35b49b04b4f159fb1ccc5ae29978c5f
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-24 22:08:42 +0530

    make changes in ec2.tf

--- examples/ec2instance-with-sg/ec2.tf
+++ examples/ec2instance-with-sg/ec2.tf
@@ -1,6 +1,9 @@
+## sg means = security group
+
 resource "aws_instance" "sample" {
   ami                                    = "ami-059e6ca6474628ef0"
   instance_type                     = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
 }
 
 provider "aws" {
@@ -9,4 +12,31 @@ provider "aws" {
 
 output "public_ip" {
   value = aws_instance.sample.public_ip
+}
+
+## aws security group terraform search in chrome and opn first info u cn find this
+
+resource "aws_security_group" "allow_ssh" {
+  name          = "allow_ssh"
+  description  = "Allow TLS inbound traffic"
+
+  ingress {
+    description             = "SSH"
+    from_port               = 22
+    to_port                   = 22
+    protocol                 = "tcp"
+    cidr_blocks            = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port              = 0
+    to_port                  = 0
+    protocol                = "-1"
+    cidr_blocks           = ["0.0.0.0/0"]
+    ipv6_cidr_blocks   = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh "
+  }
 }
