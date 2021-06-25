5aebb3710850d6e586d01245c3fb883fbc1b4608
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-25 21:23:25 +0530

    make changes in roboshop

--- roboshop/ec2.tf
+++ roboshop/ec2.tf
@@ -3,8 +3,8 @@
 //}
 
 module "ec2" {
-  source                           = "./ec2"
-  COMPONENTS               = var.COMPONENTS
+  source                     = "./ec2"
+  COMPONENTS        = var.COMPONENTS
 }
 
 ## then go to template.tf and give output to use this module(ec2) by using new  launch-template_id
