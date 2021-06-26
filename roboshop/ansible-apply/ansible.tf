96c401f78b3a626cc8245d79877680b08d687976
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-26 11:57:37 +0530

    make changes in ansible.tf

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -21,4 +21,6 @@ resource "null_resource" "ansible-apply" {
   }
 }
 
-variable "COMPONENTS" {}
+variable "COMPONENTS" {
+  default = ["frontend", "mongodb", "catalogue", "redis", "user", "cart", "mysql", "shipping", "rabbitmq", "payment"]
+}
