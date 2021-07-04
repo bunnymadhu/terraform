c9b3d8d2307eff532051d4bf772269f598ee5ea0
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-04 22:54:26 +0530

    make changes in filebeat.yml

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -16,14 +16,24 @@ variable "COMPONENTS" {}
 ## here count is required bcz  var.COMPONENTS it will create instances from  frontend-payment
 ## and in tag==Name first placed here is sample so 10 spot requests will created in the name of sample,so by avoiding that we will put element(var.COMPONENTS)..
 
-resource "time_sleep" "wait" {
-  depends_on                         = [aws_spot_instance_request.cheap_worker]
-  create_duration                    = "120s"
+//resource "time_sleep" "wait" {
+//  depends_on                         = [aws_spot_instance_request.cheap_worker]
+//  create_duration                    = "120s"
+//}
+resource "null_resource" "wait" {
+  depends_on = [aws_spot_instance_request.cheap_worker]
+  triggers = {
+    abc = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
 }
 ## we can give time here for to create instances at that particular names...means var.COMPONENTS
 
 resource "aws_ec2_tag" "spot" {
-  depends_on                        = [time_sleep.wait]
+  depends_on                        = [null_resource.wait]
   count                                  = length(var.COMPONENTS)
   resource_id                         = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key                                     = "Name"
@@ -33,7 +43,7 @@ resource "aws_ec2_tag" "spot" {
 ## here * means it is list so 10 instances created it is going to be all the spot instances will create as a list..
 
 resource "aws_ec2_tag" "monitor" {
-  depends_on                        = [time_sleep.wait]
+  depends_on                        = [null_resource.wait]
   count                                  = length(var.COMPONENTS)
   resource_id                         = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key                                     = "Monitor"
@@ -43,7 +53,7 @@ resource "aws_ec2_tag" "monitor" {
 ## if you want to monitor any instance then keep tag in monitor=yes and here u want write above script...and apply -auto-approve it will reflectd to want we want to monitor..
 
 resource "aws_route53_record" "dns" {
-  depends_on                        = [time_sleep.wait]
+  depends_on                        = [null_resource.wait]
   count                                  = length(var.COMPONENTS)
   zone_id                               = "Z03444518JCZ2U6FF5S6"
   name                                  = "${element(var.COMPONENTS, count.index)}.roboshop.internal"
